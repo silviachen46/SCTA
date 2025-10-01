7cf207e409a539ee86a863234f2f6a802cc3afcc
silvia <shuyu5@illinois.edu>
2025-10-01 17:27:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
+from agent_v5 import AgentBase, AnnotationAgent, ResultAgent, PlanningAgent
+import time
 
+start_time = time.time()
+planning_agent = PlanningAgent()
+planning_agent.build_graph()
+planning_agent.graph.execute_graph()
+end_time = time.time()
+elapsed_time = end_time - start_time
 
-import os
-from openai import AzureOpenAI
-import pandas as pd
-import numpy as np
-import json
-import ast
-import re
-import subprocess
-import tqdm
-from agent_v2 import DataScienceAgent
+print(f"Execution time: {elapsed_time:.2f} seconds")
 
-role_prompt = """You are supposed to write python code with scanpy, numpy, pandas, matplotlib.pyplot 
-to carry out the following bioinformatics subtasks in Single Cell Analysis. If a parameter required by the function is not specified,
-You are supposed to assign a proper value according to biological commonsense."""
-
-
-ds_agent = DataScienceAgent(role_prompt=role_prompt)
-ds_agent.initialize_action_units()
-ds_agent.process_action_unit()
